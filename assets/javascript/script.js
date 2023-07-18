d53d52977e540a4dca6056d7284e0539b4ef6aff
Benjamin Bræstrup Sayoc <benj5378@outlook.com>
2023-07-18 20:01:29 +0200

    Fix overflow  menu and non-link menu items

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,8 +1,13 @@
 // All JavaScript in here is non-essential for functionality and readability. It is solely to stop some bs from happening
 
 // Insert span into all menu items for extra CSS customization
+function startUpTasks() {
+    insertSpanInMenuItems();
+    makeOpenInNewWindow();
+}
+
 function insertSpanInMenuItems() {
-    menuItems = document.querySelectorAll(".menuList li > a")
+    menuItems = document.querySelectorAll(".menuList li > a, .menuList li:not(:has(a))")
     // Wrap menu text in span
     for(let i = 0; i < menuItems.length; i++) {
         menuItems[i].innerHTML = `<span>${menuItems[i].innerHTML}</span>`;
